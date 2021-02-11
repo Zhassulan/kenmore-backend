5e317d390f80e78abb74956567630c97197d8331
Developer.KZ <developer.kz@gmail.com>
2021-02-11 23:31:43 +0600

    download is done

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -13,9 +13,9 @@ const upload = (req, res) => {
 
 const download = (req, res) => {
     const fileName = req.params.name;
-    const directoryPath = __basedir + "/resources/static/assets/uploads/";
+    const directoryPath = "resources/static/assets/uploads";
 
-    res.download(directoryPath + fileName, fileName, (err) => {
+    res.download(directoryPath + "/" + fileName, fileName, (err) => {
         if (err) {
             res.status(500).send({
                 message: "Could not download the file. " + err,
--- routes/file.routes.js
+++ routes/file.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
 
     const files = require("../controllers/file.controller.js");
 
-    app.post("/files/user/:id", files.upload);
-    app.get("/files/user/:id", files.download);
+    app.post("/files", files.upload);
+    app.get("/files/:name", files.download);
 
 };
